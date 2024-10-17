3438a77dd1f288e26ec5f64b45358d821df2ffd3
Lucas-O-S <lucasoscomercial@gmail.com>
2024-10-17 13:22:37 -0300

    SQL

---
+++ Ferramenta/SQL/SQL.sql
@@ -0,0 +1,71 @@
+use auladb
+go
+
+CREATE TABLE [dbo].[ferramentas]( 
+ Id [int] NOT NULL  primary key identity (1,1), 
+ descricao [varchar](50) NULL, 
+ FabricanteId  int ); 
+ go
+
+ create table fabricantes(
+	id int not null primary key identity(1,1),
+	nome varchar(100)
+ )
+ go
+
+ insert into fabricantes values ('Fabricante 01')
+ go
+insert into fabricantes values ('Fabricante 02')
+go
+insert into fabricantes values ('Fabricante 03')
+go
+
+ create or alter procedure sp_insert_ferramentas(
+	@descricao varchar(50),
+	@fabricanteId int
+ )
+ as
+ begin
+	insert into ferramentas (descricao, fabricanteId) values (@descricao, @fabricanteId)
+ end
+ go
+
+  create or alter procedure sp_update_ferramentas(
+	@id int,
+	@descricao varchar(50),
+	@fabricanteId int
+ )
+ as
+ begin
+	update ferramentas set descricao = @descricao, fabricanteId = @fabricanteId where id = @id
+ end
+ go
+
+  create or alter procedure sp_delete_ferrementas(
+	@id int
+ )
+ as
+ begin
+	delete ferramentas where id = @id
+ end
+ go
+
+ create or alter procedure sp_consulta_ferrementas(
+	@id int
+ )
+ as
+ begin
+	select * from ferramentas where id = @id
+ end
+ go
+
+ 
+ create or alter procedure sp_lista_ferrementas(
+	@id int
+ )
+ as
+ begin
+	select * from ferramentas
+ end
+ go
+
