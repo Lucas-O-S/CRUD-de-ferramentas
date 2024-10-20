ef875ecbd7d76aae91cc5ae13e1881698014a7c3
Lucas-O-S <lucasoscomercial@gmail.com>
2024-10-20 15:33:13 -0300

    correção

--- Crud_Pizzas/Crud_Pizzas/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Crud_Pizzas.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
